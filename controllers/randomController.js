04f870d2fec383f91f78e95926b055a9fb769c2c
DavidRigueira <david-rigi@hotmail.com>
2021-02-24 14:30:29 +0100

    primer commit

---
+++ controllers/randomController.js
@@ -0,0 +1,21 @@
+function getRandom(req, res){
+    const aleatorio = Math.floor( Math.random()*100 );
+    res.status(200).send(''+aleatorio)
+}
+
+function getRandomTo(req, res){
+    const val = req.params.val
+    const aleatorio = Math.floor( Math.random()*val );
+    res.status(200).send(''+aleatorio)
+}
+
+function getRandomBetween(req, res){
+    const min = parseInt(req.query.min);
+    const max = parseInt(req.query.max)+1;
+
+    //TOFIX ver formula
+    const aleatorio = Math.floor(Math.random() * (max - min)) + min;
+    res.status(200).send(''+aleatorio)
+}
+
+module.exports = {getRandom, getRandomTo, getRandomBetween}
